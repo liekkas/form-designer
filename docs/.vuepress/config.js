890b8f1d7991ae41aa560d40b4bd99ccce13bd10
liekkas <zjq0717@163.com>
2018-07-12 20:09:43 +0800

    update wiki

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: '表单设计器',
   description: '一款采用Vue框架基于iView组件库的表单设计器',
   base: '/form-designer/wiki/',
-  dest: '../wiki/',
+  dest: './wiki/',
   head: [
     ['link', { rel: 'icon', href: `/logo.png` }]
   ],
